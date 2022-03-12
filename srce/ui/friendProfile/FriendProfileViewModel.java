202802776df8f18eaa5bdbab63deb8355df2e790
Amit Kumar <33752734+skyamit@users.noreply.github.com>
2022-03-13 01:55:24 +0530

    Add files via upload

---
+++ srce/ui/friendProfile/FriendProfileViewModel.java
@@ -0,0 +1,7 @@
+package amit.example.lilichatians.ui.friendProfile;
+
+import androidx.lifecycle.ViewModel;
+
+public class FriendProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ srce/ui/setting/SettingViewModel.java
@@ -0,0 +1,7 @@
+package amit.example.lilichatians.ui.setting;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
